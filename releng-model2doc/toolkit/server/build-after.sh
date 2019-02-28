7bd70215e83e607d90bfc50b2029b9da035b50e1
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2019-02-28 15:49:31 +0100

    [Releng] share script build-after for model2doc build
    Change-Id: I7bacc70f62badfce8fca2b5dcdce2de05c3025e5

---
+++ releng-model2doc/toolkit/server/build-after.sh
@@ -0,0 +1,61 @@
+#--------------------------------------------------------------------------------
+# Copyright (c) 2012-2014, 2018 CEA LIST.
+#
+#    
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#    Nicolas Bros (Mia-Software)
+#	 Camille Letavernier (CEA LIST)
+#    Vincent Lorenzo (CEA LIST) adapted for model2doc
+#--------------------------------------------------------------------------------
+
+########## publishing ##########
+
+##
+## Known variables:
+## BUILD_ID=2014-10-01_05-16-17
+##
+## For stable builds:
+## BUILD_ALIAS=M4
+##
+
+p2UpdateSiteDir=${WORKSPACE}/source/releng-model2doc/main/site/target/repository
+updateSite=${WORKSPACE}/repository
+
+if [ -n "$BUILD_ALIAS" ]; then
+  buildType=S
+else
+  buildType=N
+fi
+
+#COMPACT_BUILD_ID="${BUILD_ID//[-_]}"
+#COMPACT_BUILD_ID="${COMPACT_BUILD_ID:0:12}"
+#COMPACT_BUILD_ID="$(date +%Y%m%d%H%M)"
+COMPACT_BUILD_ID="$BUILD_TIMESTAMP"
+FULL_BUILD_ID=${buildType}${COMPACT_BUILD_ID}
+
+if [ -n "$BUILD_ALIAS" ]; then
+  updateZipName=Papyrus-Model2Doc-Update-${BUILD_ALIAS}.zip
+else
+  updateZipName=Papyrus-Model2Doc-Update-${FULL_BUILD_ID}.zip
+fi
+zipName="Papyrus-Model2Doc.zip"
+
+rm -rf tmp
+mkdir -p "tmp/$FULL_BUILD_ID"
+
+rm -rf $updateSite
+mv $p2UpdateSiteDir $updateSite
+
+# create the update site zip
+(cd $updateSite && zip -r $updateZipName *)
+mv $updateSite/$updateZipName "tmp/$FULL_BUILD_ID"
+
+(cd tmp && zip -r $zipName *)
+mv tmp/$zipName .
